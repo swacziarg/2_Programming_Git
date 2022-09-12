8aa69f94622dafabbf3ae8587bd6f64f362a2656
Anika Iyer <anika.sule.iyer@gmail.com>
2022-09-12 00:02:15 -0700

    Created Index
    Attempted to create Index, with methods Initialize, Add, and Remove. Testing in progress.

---
+++ src/Index.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Formatter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+public class Index {
+	
+	HashMap<String, String> blobMap = new HashMap<String, String>();
+	FileWriter fw;
+	String location;
+	File index;
+	
+	public Index()
+	{
+		
+	}
+	
+	public void initialize()
+	{
+		File directory = new File("./objects/");
+		directory.mkdir();
+		index = new File("index");
+	}
+	
+	public void add(String fileName) throws IOException
+	{
+		Blob blob = new Blob(fileName);
+		blobMap.put(fileName, blob.sha1);
+		PrintWriter out = new PrintWriter(new FileWriter(index));
+		for(String key : blobMap.keySet())
+		{
+			out.write(key + " : " + blobMap.get(key));
+		}
+	}
+	
+	public void remove(String fileName)
+	{
+		File toDelete = new File(blobMap.get(fileName)); 
+		toDelete.delete();
+	    blobMap.remove(fileName);
+
+	}
+
+}
---
+++ src/IndexTester.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class IndexTester {
+
+	public static void main(String[] args) throws IOException
+	{
+		
+		Index index = new Index();
+		index.initialize();
+		
+		index.add("textfile.txt");
+		index.add("textfile2.txt");
+		index.add("textfile3.txt");
+		
+		index.remove("textfile3.txt");
+	}
+
+}
