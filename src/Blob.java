7210581037ea3fbc0ea7941942c424fac831e586
Anika Iyer <anika.sule.iyer@gmail.com>
2022-09-11 21:38:25 -0700

    Added Generate SHA-1 Method
    Found Generate SHA-1 Method on StackOverflow, added it in. Tested it to see if it generated the right output value, it appears to do so.

---
+++ src/Blob.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Formatter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+public class Blob {
+
+	
+	
+	public Blob(String inputFile)
+	{
+		
+	}
+
+	public static String generateSHA1(String input)
+	{
+	    String sha1 = "";
+	    try
+	    {
+	        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+	        crypt.reset();
+	        crypt.update(input.getBytes("UTF-8"));
+	        sha1 = byteToHex(crypt.digest());
+	    }
+	    catch(NoSuchAlgorithmException e)
+	    {
+	        e.printStackTrace();
+	    }
+	    catch(UnsupportedEncodingException e)
+	    {
+	        e.printStackTrace();
+	    }
+	    return sha1;
+	}
+
+	public static String byteToHex(final byte[] hash)
+	{
+	    Formatter formatter = new Formatter();
+	    for (byte b : hash)
+	    {
+	        formatter.format("%02x", b);
+	    }
+	    String result = formatter.toString();
+	    formatter.close();
+	    return result;
+	}
+}
---
+++ src/BlobTester.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class BlobTester {
+	
+	public static void main(String[] args) throws IOException
+	{
+		Blob blob = new Blob("textfile.txt");
+		System.out.println(blob.generateSHA1("Hello"));
+		
+	}
+}
