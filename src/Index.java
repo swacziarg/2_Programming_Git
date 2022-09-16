9d3bd485bc27579c12f18f13d42e60f5dd7af05c
Simon Wacziarg <swacziarg@gmail.com>
2022-09-15 21:33:49 -0700

    added tree

--- src/Index.java
+++ src/Index.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,6 +8,7 @@ import java.io.IOException;
 import java.util.Formatter;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -56,6 +58,34 @@ public class Index {
 		}
 		out.close();
 
+	}
+	public void Tree(ArrayList<String> s) throws NoSuchAlgorithmException, IOException {
+		String str = s.toString();
+		
+		
+		MessageDigest md = MessageDigest.getInstance("SHA-1");
+		 
+         // digest() method is called
+         // to calculate message digest of the input string
+         // returned as array of byte
+        byte[] messageDigest = md.digest(str.getBytes());
+
+         // Convert byte array into signum representation
+        BigInteger no = new BigInteger(1, messageDigest);
+
+         // Convert message digest into hex value 
+        String hashtext = no.toString(16);
+
+         // Add preceding 0s to make it 32 bit
+        while (hashtext.length() < 32) {
+            hashtext = "0" + hashtext;
+        }
+        
+        BufferedWriter writer = new BufferedWriter(new FileWriter("objects" + "\\" +hashtext));
+        writer.write(s.toString());
+    
+        writer.close();
+        
 	}
 
 }
